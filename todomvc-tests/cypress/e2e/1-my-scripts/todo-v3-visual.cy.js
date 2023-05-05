50cdc4dbee329e9d73513ab9c2a3afd40696c97e
João Pedro <jpssam6@gmail.com>
2023-05-05 11:41:11 -0400

    Update todo-v3-visual.cy.js

--- todomvc-tests/cypress/e2e/1-my-scripts/todo-v3-visual.cy.js
+++ todomvc-tests/cypress/e2e/1-my-scripts/todo-v3-visual.cy.js
@@ -8,8 +8,8 @@ describe('trying visual validation', () => {
     
     before(() => todoPage.navigate())
     
-    // This is not working yet :(
-    // UPDATE: I figured out I forgot to set env variables lol
+    // This is not working yet :( MAY 4th 2023
+    // UPDATE: I figured out I forgot to set env variables lol MAY 5th 2023
     beforeEach(() => cy.eyesOpen({appName: "Joao's portfolio TodoMVC", batchName: "Joao's portfolio TodoMVC batch"}))
     afterEach(() => cy.eyesClose())
 
